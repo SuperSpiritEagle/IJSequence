4dcb93122ffaa62b992f51eb7c48a1ba695e8fd6
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-16 12:26:05 +0500

    init

---
+++ IJSequence/IJSequence/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IJSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+            int count = 14;
+            int beginningSequence = -2;
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{beginningSequence += number} ");
+            }
+        }
+    }
+}
